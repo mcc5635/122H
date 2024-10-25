5eb6014b8e59df998d2128dffa468b8cf7546fde
mc2 <themikechase@gmail.com>
2024-10-25 13:09:42 -0700

    portal portal portal

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import ClientButton from './clientbutton';
 
 const Hero = () => (
   <div className="hero my-3 text-center" data-testid="hero">
-    <h1 className="mb-4" data-testid="hero-title">
+    <h1 className="mb-1" data-testid="hero-title">
       122 Health
     </h1>
 
-    <p className="lead" data-testid="hero-lead">
-      Understand your health better{' '}
+    <p className="lead mb-2" data-testid="hero-lead" style={{ marginBottom: '10px' }}>
+      Understand your health better
     </p>
+    
+    <a href="https://buy.stripe.com/fZeg0tfR54t47yEeUU" target="_blank" rel="noopener noreferrer">
+      <ClientButton style={{ marginTop: '40px' }}>Sign Up</ClientButton>
+    </a>
   </div>
 );
 
